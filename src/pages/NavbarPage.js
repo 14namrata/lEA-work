a2be3f34a7cc25b70874ac84636abb397377a52c
sachingusain234 <sachingusain234@gmail.com>
2025-10-08 23:21:03 +0530

    Added User Profile

---
+++ src/pages/NavbarPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import './NavbarPage.css';
+
+const NavbarPage = () => {
+  return (
+    <div className="navbar-page">
+      <Navbar />
+    </div>
+  );
+};
+
+export default NavbarPage;
